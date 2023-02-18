14436f946097a8eb45515b2f253656a4255028a1
Epap <peia@inbox.ru>
2023-02-18 16:42:06 +0300

    Convert to decimal or from decimal

--- Number Base Converter/task/src/converter/Main.kt
+++ Number Base Converter/task/src/converter/Main.kt
@@ -1,11 +1,57 @@
 package converter
 
+var fromRadix = 10
+var toRadix = 10
+var value = ""
+
 fun main() {
 
-    println("Enter number in decimal system:")
-    val readNumber = readln().toLong()
-    println("Enter target base:")
-    val radix = readln().toInt()
-    println("Conversion result: ${readNumber.toBigInteger().toString(radix)}")
+    while(true) {
+        println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)")
+        when (readln()) {
+            "/exit" -> return
+            "/from" -> {
+                value = readDecimal()
+                fromRadix = 10
+                toRadix = readToRadix()
+            }
+            "/to" -> {
+                value = readSource()
+                toRadix = 10
+                fromRadix = readFromRadix()
+            }
+        }
+        println("Conversion ${toDecimal()}result: ${conversion(fromRadix, toRadix, value)}")
+    }
+}
+
+fun conversion(fromRadix: Int, toRadix: Int, value: String): String {
+    return value.toBigInteger(fromRadix).toString(toRadix)
 
 }
+
+fun toDecimal(): String {
+    if (toRadix == 10) return "to decimal "
+    return ""
+}
+
+fun readFromRadix(): Int {
+    println("Enter source base:")
+    return readln().toInt()
+}
+
+fun readToRadix(): Int {
+    println("Enter the target base:")
+    return readln().toInt()
+}
+
+fun readSource(): String {
+    println("Enter source number:")
+    return readln()
+}
+
+fun readDecimal(): String {
+    println("Enter a number in decimal system:")
+    return readln()
+}
+
